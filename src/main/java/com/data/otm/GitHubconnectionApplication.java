71574ee1995c245f5ba3869775be1f1b0de71ae7
DhineshKrishnaM <aerodhinesh037@gmail.com>
2022-09-02 16:43:25 +0530

    first

---
+++ src/main/java/com/data/otm/GitHubconnectionApplication.java
@@ -0,0 +1,13 @@
+package com.data.otm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubconnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubconnectionApplication.class, args);
+	}
+
+}
